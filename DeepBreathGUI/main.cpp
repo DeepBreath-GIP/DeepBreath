d268e367b8cbdcefe2461eacd5ef8ebe9ea9ea9a
NiliStein <nilistn@gmail.com>
2020-09-29 20:29:48 +0300

    re-added qt gui directory

---
+++ DeepBreathGUI/main.cpp
@@ -0,0 +1,11 @@
+#include "deepbreathgui.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    DeepBreath w;
+    w.show();
+    return a.exec();
+}
